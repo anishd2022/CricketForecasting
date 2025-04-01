1164894647094f89febb20f0afc6f6d0ef401557
Anish Deshpande <anish.d@farmx.ag>
2025-03-31 21:58:21 -0700

    renamed .pkl file and the naming convention is not hard coded anymore in game_data.py file

--- Code/game_data.py
+++ Code/game_data.py
@@ -13,11 +13,13 @@ folder_path = "Data/all_json/"
 # Initialize an empty list to store DataFrames
 df_list = []
 
+match_type = "T20"
+
 # Iterate over all files in the folder
 for filename in os.listdir(folder_path):
     if filename.endswith(".json"):  # Only process JSON files
         file_path = os.path.join(folder_path, filename)
-        df = convert_game_json_to_ballbyball_df(file_path)
+        df = convert_game_json_to_ballbyball_df(file_path, match_format=match_type)
         print(f"processed dataframe for {filename}")
         if df is not None:  # Ensure only valid DataFrames are added
             df_list.append(df)
@@ -27,7 +29,7 @@ print(f"Processed {len(df_list)} DataFrames.")
 
 
 # Path where you want to save the pickle file
-pickle_file_path = "Data/all_ball_by_ball/all_games_ballbyball_data.pkl"
+pickle_file_path = f"Data/all_ball_by_ball/all_{match_type}games_ballbyball_data.pkl"
 
 # Save the df_list to the pickle file
 with open(pickle_file_path, 'wb') as f:
