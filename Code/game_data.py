ce65de38e6d8aacc196618feeceb4102cae12bd6
Anish Deshpande <anish.d@farmx.ag>
2025-03-31 21:53:41 -0700

    added .pkl file to ball by ball data folder. This file contains all the ball by ball data for T20 matches. game_data.py now contains the code to create this .pkl file

--- Code/game_data.py
+++ Code/game_data.py
@@ -2,9 +2,35 @@
 
 # Import necessary libraries
 from helper_functions import convert_game_json_to_ballbyball_df
+import pandas as pd
+import os
+import pickle
 
 
-# Example usage
-filename = "Data/all_json/336023.json"
-df = convert_game_json_to_ballbyball_df(filename)
-print(df)
+# path to JSON files:
+folder_path = "Data/all_json/"
+
+# Initialize an empty list to store DataFrames
+df_list = []
+
+# Iterate over all files in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith(".json"):  # Only process JSON files
+        file_path = os.path.join(folder_path, filename)
+        df = convert_game_json_to_ballbyball_df(file_path)
+        print(f"processed dataframe for {filename}")
+        if df is not None:  # Ensure only valid DataFrames are added
+            df_list.append(df)
+
+# Now df_list contains DataFrames, one for each match
+print(f"Processed {len(df_list)} DataFrames.")
+
+
+# Path where you want to save the pickle file
+pickle_file_path = "Data/all_ball_by_ball/all_games_ballbyball_data.pkl"
+
+# Save the df_list to the pickle file
+with open(pickle_file_path, 'wb') as f:
+    pickle.dump(df_list, f)
+    
+print(f"df_list saved to {pickle_file_path}")
